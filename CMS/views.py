5e7da2cbbf7acfd62e711beaa0e5e9c2b4ab1b9b
Apuri-Amarnath <apuriamarnath123@gmail.com>
2023-10-22 03:39:44 +0530

    add content and design in myposts.html

--- CMS/views.py
+++ CMS/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.views import View
-from .forms import UserRegisterForm
-from django.views.generic import ListView,DetailView
+from .forms import UserRegisterForm, UserLoginForm
+from django.views.generic import ListView, DetailView
 from .models import Blog_post
+from django.contrib.auth.views import LoginView
+
 
 class RegisterView(View):
     def get(self, request):
@@ -10,17 +13,34 @@ class RegisterView(View):
         return render(request, 'Users/register.html', {'form': form})
 
     def post(self, request):
-        form = UserRegisterForm(request.POST)
+        form = UserLoginForm(request.POST)
 
         if form.is_valid():
             form.save()
-            return redirect('index')
+        return redirect('index')
+
+
+class CustomLoginView(LoginView):
+    form_class = UserLoginForm
+    template_name = 'users/login.html'
+
 
 class BlogListView(ListView):
     model = Blog_post
     queryset = Blog_post.objects.all().order_by('-modified')
     template_name = 'index.html'
     context_object_name = 'blog_posts'
+
+
 class Detail_Blogpost_view(DetailView):
     model = Blog_post
     template_name = 'blog_post.html'
+
+
+class MyPostListView(ListView):
+    model = Blog_post
+    template_name = 'users/myposts.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        return Blog_post.objects.filter(author=self.request.user).order_by('author__username')
